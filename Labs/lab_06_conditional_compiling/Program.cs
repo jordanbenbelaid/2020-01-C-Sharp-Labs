6f3c55052220eccf6df31170215e1e8afb0676b1
Jordan Benbelaid <JBenbelaid@spartaglobal.com>
2020-01-20 15:12:49 +0000

    Added lab 6

---
+++ Labs/lab_06_conditional_compiling/Program.cs
@@ -0,0 +1,24 @@
+﻿#define THISISTRUE
+
+using System;
+
+namespace lab_06_conditional_compiling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is always printed");
+
+#if DEBUG
+            Console.WriteLine("This is debug code");
+#else
+            Console.WriteLine("This is a production code");
+#endif
+
+#if THISISTRUE
+            Console.WriteLine("This is true");
+#endif
+        }
+    }
+}
