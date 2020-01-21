382e810b157262cad3afee19286e7ec025970754
Jordan Benbelaid <JBenbelaid@spartaglobal.com>
2020-01-21 10:54:03 +0000

    OOP, argument arrays and literal objects

---
+++ Labs/lab_08_literal_objects/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace lab_08_literal_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dont do this
+            var string01 = new string("Here is a string");
+            //do this
+            var string02 = "here also is a string";
+
+            //array : fixed in size (Immutable)
+            //long way
+            var array01 = new int[5]; //empty memory space to hold 5 integers
+            var array02 = new int[] { 10, 20, 30, 40, 50 };   //literally defining the numbers
+
+            var list01 = new List<int>();
+            var list02 = new List<int>() { 10, 20, 30, 40, 50 };   //literals
+
+            //random object
+            dynamic object01 = new ExpandoObject();
+            object01.name = "Fred";
+            object01.age = 22;
+            object01.balance = 32.50;
+            Console.WriteLine($"object01 has name {object01.name} and age {object01.age}" + $"with balance {object01.balance}");
+
+            // creating object as a literal
+            var object02 = new
+            {
+                name = "Fred",
+                age = 22
+            };
+            Console.WriteLine($"{object02.name} has age {object02.age}");
+
+            //OOP language : create new object using literal data
+            var dog01 = new Dog()
+            {
+                name = "Fido",
+                age = 4
+            };
+            Console.WriteLine($"{dog01.name} is {dog01.age}");
+        }
+    }
+
+    class Dog
+    {
+        public string name;
+        public int age;
+    }
+}
---
+++ Labs/lab_09_OOP_intro/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace lab_09_OOP_intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var keyword infers the type from the right ie Car
+            //car01 is the instance ie particular object created
+            //Car = template used
+            //() means run a method when calling new keyword //constructor method
+            var car01 = new Car();
+            car01.Make = "Mercedes";
+
+            for (int i = 0; i < 1000; i++)
+            {
+                //Create 1000 cars
+                var car = new Car();
+            }
+
+            var newCar = new Car();
+            Console.WriteLine($"Initial speed {newCar.Speed}");
+            newCar.Speed++;
+            newCar.Speed++;
+            Console.WriteLine($"Final speed {newCar.Speed}");
+
+            var car02 = new Car("Mercedes", "C220", "Silver", 2200);
+        }
+    }
+
+    class Car
+    {
+        public string Make;
+        public string Model;
+        public string Colour;
+        public int Length; //mm
+        public int Speed;
+
+        //constructor is present even if not stated
+        //default constructor is
+        public Car() 
+        {
+            this.Speed = 0;  // this keyword refers to Instance Object in use at the time        
+        }
+
+        public Car(string make, string model, string colour, int length)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Colour = colour;
+            this.Length = length;
+            this.Speed = 0;        
+
+        }
+    }
+}
