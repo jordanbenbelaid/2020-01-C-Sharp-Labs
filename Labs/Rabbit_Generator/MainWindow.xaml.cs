6c5106a69210524742f7b0cde88144b247102cb8
Jordan Benbelaid <jbenbelaid@spartaglobal.com>
2020-01-26 20:06:17 +0000

    Finalised rabbit generator code

--- Labs/Rabbit_Generator/MainWindow.xaml.cs
+++ Labs/Rabbit_Generator/MainWindow.xaml.cs
@@ -22,9 +22,19 @@ namespace Rabbit_Generator
     {
 
         static List<Rabbit> RabbitList = new List<Rabbit>();
+        static List<Rabbit> RabbitList2 = new List<Rabbit>();
+        static List<Rabbit> RabbitList3 = new List<Rabbit>();
+        static List<Rabbit> RabbitList4 = new List<Rabbit>();
+        static Rabbit firstRabbit = new Rabbit();
         public MainWindow()
         {
             InitializeComponent();
+
+            firstRabbit.Age = 0;
+            firstRabbit.Name = "Rabbit" + 1;
+            RabbitList3.Add(firstRabbit);
+            RabbitList4.Add(firstRabbit);
+
         }
 
         private void Button100Rabbits_Click(object sender, RoutedEventArgs e)
@@ -38,7 +48,7 @@ namespace Rabbit_Generator
 
             foreach (var rabbit in RabbitList)
             {
-                ListBox100Rabbits.Items.Add(rabbit.Name+" "+rabbit.Age);
+                ListBox100Rabbits.Items.Add(rabbit.Name);
             }
         }
 
@@ -46,17 +56,31 @@ namespace Rabbit_Generator
 
         private void ButtonAge100Rabbits_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 50; i++)
+            ListBoxAgeRabbits.Items.Clear();
+            BreedRabbits.Items.Clear();
+            RabbitsBreedAtThree.Items.Clear();
+
+            if (RabbitList2.Count < 100)
             {
-                RabbitAge(RabbitList);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    var rabbit = new Rabbit();
+                    rabbit.Age = 0;
+                    rabbit.Name = "Rabbit" + i.ToString();
+                    RabbitList2.Add(rabbit);
+                }
+
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                foreach (var item in RabbitList2)
+                {
+                    item.Age++;
+                }
             }
 
-            void RabbitAge(List<Rabbit> r)
-            {
-                r.ForEach(item => item.Age++);
-            }
-
-            foreach (var rabbit in RabbitList)
+            foreach (var rabbit in RabbitList2)
             {
                 ListBoxAgeRabbits.Items.Add($"{rabbit.Name} is {rabbit.Age} years old");
             }
@@ -65,7 +89,59 @@ namespace Rabbit_Generator
 
         private void ButtonBreedRabbits_Click(object sender, RoutedEventArgs e)
         {
+            ListBoxAgeRabbits.Items.Clear();
+            BreedRabbits.Items.Clear();
+            RabbitsBreedAtThree.Items.Clear();
 
+            if (RabbitList3.Count < 65)
+            {
+
+                foreach (var item in RabbitList3)
+                {
+                    item.Age++;
+                }
+
+                foreach (var rabbit in RabbitList3.ToArray())
+                {
+                    var breedRabbit = new Rabbit();
+                    breedRabbit.Age = 0;
+                    breedRabbit.Name = "New Rabbit" + (RabbitList3.Count + 1);
+                    RabbitList3.Add(breedRabbit);
+                }
+            }
+
+            foreach (var item in RabbitList3)
+            {
+                BreedRabbits.Items.Add(item.Name + $" is {item.Age} years old");
+            }
+        }
+
+        private void ButtonRabbitsBreedAtThree_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxAgeRabbits.Items.Clear();
+            BreedRabbits.Items.Clear();
+            RabbitsBreedAtThree.Items.Clear();
+
+            foreach (var item in RabbitList4.ToArray())
+            {
+                item.Age++;
+            }
+
+            foreach (var item1 in RabbitList4.ToArray())
+            {
+                if (item1.Age > 2)
+                {
+                    var rabbit = new Rabbit();
+                    rabbit.Age = 0;
+                    rabbit.Name = "New Rabbit" + (RabbitList4.Count + 1);
+                    RabbitList4.Add(rabbit);
+                }
+            }
+
+            foreach (var newitem in RabbitList4)
+            {
+                RabbitsBreedAtThree.Items.Add(newitem.Name + " is " + newitem.Age + " years old");
+            }
         }
 
         class Rabbit
@@ -73,10 +149,5 @@ namespace Rabbit_Generator
             public int Age { get; set; }
             public string Name { get; set; }
         }
-
-
-
-
-
     }
 }
