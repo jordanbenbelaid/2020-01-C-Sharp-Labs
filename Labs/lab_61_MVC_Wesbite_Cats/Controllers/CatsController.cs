349f8014d6680f9c0e81ea5e17893926e79eba10
Jordan Benbelaid <jordanbenbelaid@hotmail.com>
2020-03-04 12:13:25 +0000

    API with MVC

---
+++ Labs/lab_61_MVC_Wesbite_Cats/Controllers/CatsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using lab_60_cats_API.Models;
+
+namespace lab_61_MVC_Wesbite_Cats.Controllers
+{
+    public class CatsController : Controller
+    {
+        private readonly CatDbContext _context;
+
+        public CatsController(CatDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cats
+        public async Task<IActionResult> Index()
+        {
+            var catDbContext = _context.Cats.Include(c => c.Category);
+            return View(await catDbContext.ToListAsync());
+        }
+
+        // GET: Cats/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cat = await _context.Cats
+                .Include(c => c.Category)
+                .FirstOrDefaultAsync(m => m.CatID == id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            return View(cat);
+        }
+
+        // GET: Cats/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryID"] = new SelectList(_context.Categories, "CategoryID", "CategoryName");
+            return View();
+        }
+
+        // POST: Cats/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CatID,CatName,CategoryID,DateOfBirth")] Cat cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cat);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryID"] = new SelectList(_context.Categories, "CategoryID", "CategoryName", cat.CategoryID);
+            return View(cat);
+        }
+
+        // GET: Cats/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cat = await _context.Cats.FindAsync(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryID"] = new SelectList(_context.Categories, "CategoryID", "CategoryName", cat.CategoryID);
+            return View(cat);
+        }
+
+        // POST: Cats/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CatID,CatName,CategoryID,DateOfBirth")] Cat cat)
+        {
+            if (id != cat.CatID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cat);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CatExists(cat.CatID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryID"] = new SelectList(_context.Categories, "CategoryID", "CategoryName", cat.CategoryID);
+            return View(cat);
+        }
+
+        // GET: Cats/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cat = await _context.Cats
+                .Include(c => c.Category)
+                .FirstOrDefaultAsync(m => m.CatID == id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            return View(cat);
+        }
+
+        // POST: Cats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cat = await _context.Cats.FindAsync(id);
+            _context.Cats.Remove(cat);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CatExists(int id)
+        {
+            return _context.Cats.Any(e => e.CatID == id);
+        }
+    }
+}
---
+++ Labs/lab_61_MVC_Wesbite_Cats/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lab_61_MVC_Wesbite_Cats.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
